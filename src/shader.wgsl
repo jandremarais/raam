c3aa194730094172c39b3af08b94c3f8a78860bf
Jan Marais <maraisjandre9@gmail.com>
2024-03-05 20:44:43 +0200

    get window showing

--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,47 +1,23 @@
 struct VertexInput {
 	@location(0) position: vec2<f32>,
-	// @builtin(instance_index) instance: u32,
 }
 
 struct VertexOutput {
 	@builtin(position) clip_position: vec4<f32>,
 };
 
-struct Locals {
-    screen_size: vec2<f32>,
-	offset: vec2<f32>,
-    // Uniform buffers need to be at least 16 bytes in WebGL.
-    // See https://github.com/gfx-rs/wgpu/issues/2072
-    _padding: vec2<u32>,
-};
-@group(0) @binding(0) var<uniform> r_locals: Locals;
-
-fn position_from_screen(screen_pos: vec2<f32>) -> vec4<f32> {
-    return vec4<f32>(
-        2.0 * (screen_pos.x + r_locals.offset.x )  / r_locals.screen_size.x - 1.0,
-        1.0 - 2.0 * (screen_pos.y + r_locals.offset.y ) / r_locals.screen_size.y,
-        0.0,
-        1.0,
-    );
-}
 
 @vertex
 fn vs_main(
 	model: VertexInput,
 ) -> VertexOutput {
 	var out: VertexOutput;
-	// let i = f32(model.instance);
- //    let cell = vec2f(i % grid_size.grid_size.x, floor(i / grid_size.grid_size.x));
-
- //    let cellOffset = cell / grid_size.grid_size * 2.0;
- //    let gridPos = (vec2<f32>(model.position.x, model.position.y) + 1.0) / grid_size.grid_size - 1.0 + cellOffset;
-	out.clip_position = position_from_screen(model.position);
+	out.clip_position = vec4f(model.position, 0.0, 1.0);
 	return out;
 }
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
 	return vec4f(0.39676, 0.46778, 0.82279, 1.0);
-	// return vec4<f32>(0.3, 0.1, 0.1, 1.0);
 }
 
--- src/state.rs
+++ src/state.rs
@@ -63,160 +63,48 @@ impl<'a> State<'a> {
 
         surface.configure(&device, &config);
 
-        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-        });
+        // let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        //     label: Some("Shader"),
+        //     source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        // });
 
-        // let swapchain_capabilities = surface.get_capabilities(&adapter);
-        // let swapchain_format = swapchain_capabilities.formats[0];
-        let offset = [0.0, 0.0];
-        let locals_uniform = LocalsUniform {
-            screen_size: [size.width as f32, size.height as f32],
-            offset,
-            _padding: [0., 0.],
-        };
+        // let render_pipeline_layout =
+        //     device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        //         label: Some("Render Pipeline Layout"),
+        //         bind_group_layouts: &[],
+        //         push_constant_ranges: &[],
+        //     });
 
-        let locals_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Locals Buffer"),
-            contents: bytemuck::cast_slice(&[locals_uniform]),
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-        });
-
-        let locals_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::VERTEX,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-                label: Some("locals_bind_group_layout"),
-            });
-
-        let locals_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &locals_bind_group_layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: locals_buffer.as_entire_binding(),
-            }],
-            label: Some("camera_bind_group"),
-        });
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[&locals_bind_group_layout],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main",
-                buffers: &[Vertex::desc()],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                // cull_mode: Some(wgpu::Face::Back),
-                cull_mode: None,
-                polygon_mode: wgpu::PolygonMode::Fill,
-                unclipped_depth: false,
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState::default(),
-            multiview: None,
-        });
-
-        let mut vertices = Vec::new();
-
-        let line_width = 2.0;
-        let row_height = 40.0;
-        let col_width = 80.0;
-        let x_start = 10.;
-        let x_end = 400.;
-        let y_start = 10.;
-        let y_end = 2000.;
-        // for row in 0..data.len() + 1 {
-        for row in 0..400 {
-            let i = row as f32;
-            let y_top = y_start + i * row_height;
-            let y_bot = y_start + (i * row_height + line_width);
-            let row_vert = &[
-                Vertex {
-                    position: [x_start, y_top],
-                },
-                Vertex {
-                    position: [x_end, y_top],
-                },
-                Vertex {
-                    position: [x_start, y_bot],
-                },
-                Vertex {
-                    position: [x_end, y_top],
-                },
-                Vertex {
-                    position: [x_start, y_bot],
-                },
-                Vertex {
-                    position: [x_end, y_bot],
-                },
-            ];
-            vertices.extend_from_slice(row_vert);
-        }
-
-        for col in 0..100 {
-            let i = col as f32;
-            let x_left = x_start + i * col_width;
-            let x_right = x_start + (i * col_width + line_width);
-            let col_vert = &[
-                Vertex {
-                    position: [x_left, y_start],
-                },
-                Vertex {
-                    position: [x_right, y_start],
-                },
-                Vertex {
-                    position: [x_left, y_end],
-                },
-                Vertex {
-                    position: [x_right, y_start],
-                },
-                Vertex {
-                    position: [x_left, y_end],
-                },
-                Vertex {
-                    position: [x_right, y_end],
-                },
-            ];
-            vertices.extend_from_slice(col_vert);
-        }
-
-        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex buffer"),
-            contents: bytemuck::cast_slice(&vertices),
-            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
-        });
-
-        let num_vertices = vertices.len() as u32;
+        // let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        //     label: Some("Render Pipeline"),
+        //     layout: Some(&render_pipeline_layout),
+        //     vertex: wgpu::VertexState {
+        //         module: &shader,
+        //         entry_point: "vs_main",
+        //         buffers: &[],
+        //     },
+        //     fragment: Some(wgpu::FragmentState {
+        //         module: &shader,
+        //         entry_point: "fs_main",
+        //         targets: &[Some(wgpu::ColorTargetState {
+        //             format: config.format,
+        //             blend: Some(wgpu::BlendState::REPLACE),
+        //             write_mask: wgpu::ColorWrites::ALL,
+        //         })],
+        //     }),
+        //     primitive: wgpu::PrimitiveState {
+        //         topology: wgpu::PrimitiveTopology::TriangleList,
+        //         strip_index_format: None,
+        //         front_face: wgpu::FrontFace::Ccw,
+        //         cull_mode: None,
+        //         polygon_mode: wgpu::PolygonMode::Fill,
+        //         unclipped_depth: false,
+        //         conservative: false,
+        //     },
+        //     depth_stencil: None,
+        //     multisample: wgpu::MultisampleState::default(),
+        //     multiview: None,
+        // });
 
         Self {
             surface,
@@ -224,15 +112,6 @@ impl<'a> State<'a> {
             queue,
             config,
             size,
-            cursor_pos: None,
-            offset,
-            render_pipeline,
-            vertex_buffer,
-            locals_bind_group,
-            locals_buffer,
-            locals_uniform,
-            num_vertices,
-            // data,
         }
     }
 
